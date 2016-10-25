0e3ce6dff72ced237c5927fe21c8a71479737a68
JHOWELL <atlas3141@gmail.com>
2016-10-25 16:05:43 -0700

    Command Line Loop and Methods

--- MediaDatabase.cpp
+++ MediaDatabase.cpp
@@ -1,11 +1,43 @@
 # include <iostream>
-//# include "genericType.h"
+# include <vector>
+# include <cstring>
+# include "genericType.h"
 # include "movie.h"
 # include "videoGame.h"
 # include "music.h"
+
 using namespace std;
 
+void newItem(vector<genericType*>* list);
+void searchItems(vector<genericType*>* list);
+
 int main(){
-  movie* c = new movie();
-  cout << c->getTitle() << endl;
+  vector<genericType*> list;
+  char input[20];
+  bool running = true;
+  while (running == true){ //command line loop
+    cin.get(input,20);
+    cin.ignore();
+    if (!strcmp(input,"SEARCH")){
+      searchItems(& list);
+    }
+    else if (!strcmp(input, "ADD")){
+      newItem(& list);
+    }
+    else if (!strcmp(input, "HELP")){
+      cout << "Commands: \nPSearch\nADD\nExit" << endl;
+    }
+    else if (!strcmp(input, "EXIT")){
+      running = false;
+    }
+    else{
+      cout << "No such command: " <<  input << endl;
+      cout << "Type HELP for a list of commands" << endl;
+    }
+  }
+}
+void searchItems(vector<genericType*>* list){ 
+}
+void newItem(vector<genericType*>* list){
+  
 }
